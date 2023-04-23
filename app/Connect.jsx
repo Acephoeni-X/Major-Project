fe3acbbff4ef04b8722581847024de7fbfcc6711
Rishi Sharma <sharma.rishix02@gmail.com>
2023-04-23 09:05:33 +0530

    merge tushar-ui-update fix

--- app/Connect.jsx
+++ app/Connect.jsx
@@ -2,10 +2,6 @@
 import getAccountAdd from "@/utils/getAccountAdd";
 import getBalance from "@/utils/getBalance";
 import React, { useContext, useEffect, useState } from "react";
-<<<<<<< HEAD
-=======
-import GotoDashboard from "@/components/GotoDashboard";
->>>>>>> tushar-ui-update
 import { useRouter } from "next/navigation";
 import { QResponse } from "./Context/QRRes";
 
@@ -20,23 +16,13 @@ const LoginScreen = () => {
     if (window.ethereum != undefined) {
       let address = await getAccountAdd();
       setwalletAddress(address);
-<<<<<<< HEAD
-=======
-
->>>>>>> tushar-ui-update
       setbalance(await getBalance(address));
     }
   };
 
   useEffect(() => {
     if (walletAddress && balance) {
-<<<<<<< HEAD
       setWalletAdd(`${walletAddress}`);
-=======
-      console.log(typeof walletAddress);
-      setWalletAdd(`${walletAddress}`);
-      // console.log(walletAdd);
->>>>>>> tushar-ui-update
       router.push(`/dashboard/`);
     }
   }, [walletAddress, balance]);
@@ -57,7 +43,6 @@ const LoginScreen = () => {
   return (
     <div>
       {walletAddress && balance ? (
-<<<<<<< HEAD
         <>
           <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Redirecting...
@@ -66,20 +51,11 @@ const LoginScreen = () => {
       ) : (
         <button
           className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-=======
-        <></>
-      ) : (
-        <button
->>>>>>> tushar-ui-update
           onClick={() => {
             handleConnectWallet();
           }}
         >
-<<<<<<< HEAD
-          {"Connect Metamask"}
-=======
           Connect MetaMask
->>>>>>> tushar-ui-update
         </button>
       )}
     </div>

d767688323eb205660503602abbcf09566ee482b
Rishi Sharma <sharma.rishix02@gmail.com>
2023-05-19 16:48:09 +0530

    add data to start.bat

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,21 +1,57 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+// SPDX-License-Identifier: MIT
+// pragma solidity 0.8.17;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    string message;
-
-    constructor(string memory initalMessage) {
-        message = initalMessage;
+contract EtherTransfer {
+    struct Transaction {
+        address from;
+        address to;
+        uint256 amount;
+        string message;
     }
 
-    function getMessage() public view returns (string memory) {
-        return message;
+    mapping(uint256 => Transaction) private transactions;
+    uint256 private transactionCount;
+
+    constructor() {
+        transactionCount = 0;
     }
 
-    function sendEther(address payable _to) public payable {
-        _to.transfer(address(this).balance);
+    function transferEther(
+        address payable _to,
+        uint256 _amount,
+        string memory _message
+    ) public payable {
+        require(msg.value == _amount, "Amount should be equal to msg.value");
+        require(
+            address(this).balance >= _amount,
+            "Contract doesn't have enough balance to transfer"
+        );
+
+        _to.transfer(_amount);
+        transactions[transactionCount] = Transaction(
+            msg.sender,
+            _to,
+            _amount,
+            _message
+        );
+        transactionCount++;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+    function getTransaction(
+        uint256 _index
+    ) public view returns (address, address, uint256, string memory) {
+        require(_index < transactionCount, "Invalid index");
+
+        Transaction memory transaction = transactions[_index];
+        return (
+            transaction.from,
+            transaction.to,
+            transaction.amount,
+            transaction.message
+        );
     }
 }
